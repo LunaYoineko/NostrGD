dd8f0af43091a13c05dd8377bed45a8077bcd8a8
宵猫ルナ <stella1029.star@gmail.com>
2026-06-20 12:12:49 +0900

    Update WebClient

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781923676|41576570998';
+const CACHE_VERSION = '1781925149|43049700382';
 /** @type {string} */
 const CACHE_PREFIX = 'GDNostr-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
